aeff73b4f37a32b9e01e54c9ebbc24c0e28d4be5
Bhavik0107 <bhoirbp@gmail.com>
2020-02-23 21:07:13 -0500

    Eog-React-Assessment

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import sagas from "./sagas";
+import weatherReducer from "./reducers/Weather";
+import DashboardReducer from "./reducers/Dashboard";
+
+const reducer = combineReducers(
+  {
+    weather: weatherReducer,
+    dashboard: DashboardReducer
+  }
+);
+
+export default () => {
+  const composeEnhancers = composeWithDevTools({});
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = applyMiddleware(sagaMiddleware);
+  const store = createStore(reducer, composeEnhancers(middlewares));
+
+  sagaMiddleware.run(sagas);
+
+  return store;
+};
